8dc326ad078dc16652a95dda6c6c8c94b1a48f8a
AmalAlby <amalalbyn@gmail.com>
2022-11-28 23:16:05 +0530

    read values created

--- src/app/patientview/patientview.component.ts
+++ src/app/patientview/patientview.component.ts
@@ -7,27 +7,5 @@ import { Component } from '@angular/core';
 })
 export class PatientviewComponent {
 
-  name=""
-  id=""
-  address=""
-  numb=""
-  dname=""
-  doa=""
-  image=""
-
-  readvalues=()=>{
-
-    let data:any={
-    "name":this.name,
-    "id":this.id,
-    "address":this.address,
-    "numb":this.numb,
-    "dname":this.dname,
-    "doa":this.doa,
-    "image":this.image
-    }
-    console.log(data)
-
-  }
 
 }
